86f33992dfa0261a97a1beb5fcaec995aee127b4
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-10 14:27:54 +0530

    Update __init__.py

--- NandhaBot/__init__.py
+++ NandhaBot/__init__.py
@@ -11,7 +11,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 
 
 ### Client Run Code ###
-plugins = dict(root="NandhaBot/pligins")
+plugins = dict(root="NandhaBot")
 
 bot = Client(name="nandhabot", 
              api_id=API_ID, 
