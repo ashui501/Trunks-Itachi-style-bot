c9e801708bd5bb637d50aa39c39ae247e25168da
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-10 19:17:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 
 COMMANDS = [".","?","/","!","$","#","~",]
-GROUP = -1001717881477
+GROUP_ID = -1001717881477
+GROUP_URL = "t.me/nandhasupport"
+CHANNEL_ID = -1001566403901
+CHANNEL_URL = "t.me/nandhabots"
 
