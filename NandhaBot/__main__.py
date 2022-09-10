2dbf068f98f54b9a6490eab1c501532a8be2ebb1
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-10 14:54:29 +0530

    Update __main__.py

--- NandhaBot/__main__.py
+++ NandhaBot/__main__.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
      bot.run()
      photo_url = "http://telegra.ph/file/103f51de685933820f969.jpg"
      with bot:
-        bot.send_message(-1001724369346,text="<b>I'm Awake Already!</b>")
+        bot.send_message(-1001724369346,photo=photo_url,caption="<b>I'm Awake Already!</b>")
