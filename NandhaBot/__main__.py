450ff115e48e96d769448ca90886001a65e7abee
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-10 16:42:40 +0530

    Update __main__.py

--- NandhaBot/__main__.py
+++ NandhaBot/__main__.py
@@ -18,7 +18,7 @@ async def start(_, message):
        if message.chat.type == ChatType.PRIVATE and not await is_user(info.id):
            await add_user(info.id)
            user_count = len(await get_users())                                             
-           await bot.send_message(chat_id, text.NEW_USERS.format(info.id,info.mention,user_count))                                           :
+           await bot.send_message(chat_id=chat_id, text.NEW_USERS.format(info.id,info.mention,user_count))                                           :
            await message.reply_text("<b> Nani boi? </b>")
        elif message.chat.type == ChatType.PRIVATE and (await is_user(user_id):    
             return await message.reply_text("<b> ok boi </b>")                                           
