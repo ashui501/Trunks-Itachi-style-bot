dca66aaca1fd4f1a02fbec861206585d076d0aea
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-10 14:52:35 +0530

    Update __main__.py

--- NandhaBot/__main__.py
+++ NandhaBot/__main__.py
@@ -3,9 +3,11 @@ from pyrogram import filters
 from pyrogram.types import *
 
 
+@bot.on_message(filters.command("
+
 
 if __name__ == "__main__":
      bot.run()
      photo_url = "http://telegra.ph/file/103f51de685933820f969.jpg"
      with bot:
-        bot.send_photo(chat_id=-1001724369346,photo=photo_url,caption="I'm Awake Now!")
+        bot.send_message(-1001724369346,text="<b>I'm Awake Already!</b>")
