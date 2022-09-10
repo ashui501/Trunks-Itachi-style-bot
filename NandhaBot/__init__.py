cde5ca32f78f97b440d1d6b3a57876bc7de5de57
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-10 13:56:42 +0530

    Update __init__.py

--- NandhaBot/__init__.py
+++ NandhaBot/__init__.py
@@ -4,18 +4,15 @@ import os , time
 from telegraph import Telegraph
 StartTime = time.time()
 
-
-
+MOD_LOAD = []
+MOD_NOLOAD = []
 
 #main vars set your deploying app
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 bot = Client(name="Mikasa", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="{}/plugins".format(name)))
-
+bot.start()
 
 print("Bot is Working")
 
-help_message = []
-telegraph = Telegraph()
-telegraph.create_account(short_name=BOT_USERNAME)
