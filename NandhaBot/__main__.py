0466b070b4600bc82c2034baa74825441c315a3d
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-10 19:53:40 +0530

    Update __main__.py

--- NandhaBot/__main__.py
+++ NandhaBot/__main__.py
@@ -24,10 +24,10 @@ async def start(_, message):
            await add_user(info.id)
            user_count = len(await get_users())                                                                                     
            await message.reply_text(
-                  """<b>Happy to See you {} ↖(^ω^)↗</b>\n<b>• Read the help menu for about futures.</b>""".format(info.mention),reply_markup=InlineKeyboardMarkup(BUTTONS))                                         
+                  """<b>Happy to See you {} ↖(^ω^)↗</b>\n\n<b>• Read the help menu for about my futures. /help.</b>""".format(info.mention),reply_markup=InlineKeyboardMarkup(BUTTONS))                                         
            await bot.send_message(chat_id, text=text.NEW_USERS.format(info.id, info.mention, user_count))
        elif message.chat.type == ChatType.PRIVATE and await is_user(info.id):  
-            return await message.reply_text("""<b>Happy to See you {}</b>\n<b>• Read the help menu for about futures.</b>""".format(info.mention),reply_markup=InlineKeyboardMarkup(BUTTONS))                                         
+            return await message.reply_text("""<b>Happy to See you {}</b>\n\n<b>• Read the help menu for about my futures /help.</b>""".format(info.mention),reply_markup=InlineKeyboardMarkup(BUTTONS))                                         
        elif not message.chat.type == ChatType.PRIVATE:                                           
            return await message.reply_text(random.choice(text.GROUP_START_TEXT))
 
