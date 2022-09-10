d287c4b6303dfef804ae973823cd8c00300ea161
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-10 20:56:13 +0530

    Update help_commands.py

--- NandhaBot/plugins/help_commands.py
+++ NandhaBot/plugins/help_commands.py
@@ -5,10 +5,18 @@ from pyrogram.types import *
 from NandhaBot import bot
 
 HELP_BUTTONS = [[InlineKeyboardButton(text="YT/DL", callback_data="youtube"),]]
+BACK_HELP = [[InlineKeyboardButton(text="BACK TO HELP MENU", callback_data="help"),]]
+      
+   
+
+@bot.on_message(filters.command("help",config.COMMANDS))
+async def help(_, message):
+     await message.reply_text(text.HELP_TEXT,reply_markup=InlineKeyboardMarkup(HELP_BUTTONS))
+      
 
 @bot.on_callback_query()
-def help(_, query):
-   if query.data == "about":
+def helpdata(_, query):
+   if query.data == "help":
       query.message.edit_caption(text.HELP_TEXT,reply_markup=InlineKeyboardMarkup(HELP_BUTTONS))
    elif query.data == "youtube":
-       query.message.edit_caption(text.YT_HELP.format(text.NANDHA))
+       query.message.edit_caption(text.YT_HELP.format(text.NANDHA),reply_markup=InlineKeyboardMarkup(HELP_BUTTONS))
