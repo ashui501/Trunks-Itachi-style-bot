a547a782af5ff552a5d06b31702db80fa2e5ebfd
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-10 16:28:33 +0530

    Update __main__.py

--- NandhaBot/__main__.py
+++ NandhaBot/__main__.py
@@ -5,12 +5,21 @@ from NandhaBot import bot
 from pyrogram import filters
 from pyrogram.types import *
 from pyrogram.enums import *
+from NandhaBot.helpers.dbfunctions import (
+               user_add, is_user )
+       
 
 
 @bot.on_message(filters.command("start"))
 async def start(_, message):
-       if message.chat.type == ChatType.PRIVATE:
-           return await message.reply_text("<b> Nani boi? </b>")
+       user_id = message.from_user.id
+       chat_id = message.chat.id
+       if message.chat.type == ChatType.PRIVATE and not (await is_user(user_id):
+           await add_user(user_id)
+           await bot.send_message(chat_id, text.NEW_USERS)                                             :
+           await message.reply_text("<b> Nani boi? </b>")
+       elif message.chat.type == ChatType.PRIVATE and (await is_user(user_id):    
+            return await message.reply_text("<b> ok boi </b>")                                           
        elif not message.chat.type == ChatType.PRIVATE:
            return await message.reply_text(random.choice(text.GROUP_START_TEXT))
 
