52f6a6c6209b300e9c053211d94d431069e41a46
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-10 14:08:42 +0530

    Update __init__.py

--- NandhaBot/__init__.py
+++ NandhaBot/__init__.py
@@ -1,18 +1,21 @@
 from pyrogram import filters , Client
 import os , time
 
-
 StartTime = time.time()
 
-MOD_LOAD = []
-MOD_NOLOAD = []
 
-#main vars set your deploying app
+### Add This Vars On Your Deploying App. ###
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-bot = Client(name="Mikasa", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="{}/plugins".format(name)))
-bot.start()
 
-print("Bot is Working")
+
+### Client Run Code ###
+plugins = dict(root="NandhaBot/pligins")
+
+bot = Client(name="nandhabot", 
+             api_id=API_ID, 
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN,
+             plugins=plugins)
 
