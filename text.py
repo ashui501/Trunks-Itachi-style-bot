35e282f6d06d45de26577c5f9d2e60dabac97439
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-10 16:03:39 +0530

    Update text.py

--- text.py
+++ text.py
@@ -5,7 +5,7 @@ HELP_TEXT = """
 
 START_IMAGE = ""
 
-GROUP_START_TEXT = ( "<b>Nani Kawa Yo 〣( ºΔº )〣?<b>",
+GROUP_START_TEXT = ( "<b>Nani Kawa Yo 〣( ºΔº )〣?</b>",
                      "<b>What's Matters   ¯\(°_o)/¯ ? </b>",
                      "<b>I Already Awake.  ( • ̀ω•́  )! </b>",
                      "<b>Hey What Now?     ¯\_(ツ)_/¯ </b>")
