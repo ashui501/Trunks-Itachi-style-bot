391a279bdcfdd81f2aeaf2749c5b554157e2518b
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-10 16:00:41 +0530

    Update __main__.py

--- NandhaBot/__main__.py
+++ NandhaBot/__main__.py
@@ -12,11 +12,11 @@ async def start(_, message):
        if message.chat.type == ChatType.PRIVATE:
            return await message.reply_text("<b> Nani boi? </b>")
        elif not message.chat.type == ChatType.PRIVATE:
-           return await message.reply_text(random.chioce(text.GROUP_START_TEXT))
+           return await message.reply_text(random.choice(text.GROUP_START_TEXT))
 
 
 if __name__ == "__main__":
      bot.run()
      photo_url = "http://telegra.ph/file/103f51de685933820f969.jpg"
      with bot:
-        bot.send_message(-1001724369346,photo=photo_url,caption="<b>I'm Awake Already!</b>")
+        bot.send_photo(-1001724369346,photo=photo_url,caption="<b>I'm Awake Already!</b>")
