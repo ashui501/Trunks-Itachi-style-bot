1c36530128a511ccddf937261bcf9815798ba636
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-10 21:21:29 +0530

    Create paste.py

---
+++ NandhaBot/plugins/paste.py
@@ -0,0 +1,30 @@
+from pyrogram import filters
+from pyrogram.types import *
+from NandhaBot import bot
+from NandhaBot.helpers.utils.http import post as send
+
+@bot.on_message(filters.command("paste"))
+async def paste(_, message):
+  try:
+     BASE = "https://batbin.me/"
+     if not message.reply_to_message:
+          return await message.reply_text("<b> Reply to Document OR Message </b>")
+     elif message.reply_to_message.document:
+        doc = await m.reply_to_message.download()
+        async with aiofiles.open(doc, mode="r") as f:
+          text = await f.read()
+        os.remove(doc)
+        resp = await send(f"{BASE}api/v2/paste", data=text)
+        code = resp["message"]
+        batbin_link = f"{BASE}{code}"
+        await message.reply_photo(photo=batbin_link,caption=batbin_link)
+        
+     elif message.reply_to_message.text:
+        text = message.reply_to_message.text
+        resp = await send(f"{BASE}api/v2/paste", data=text)
+        code = resp["message"]
+        batbin_link = f"{BASE}{code}"
+        await message.reply_photo(photo=batbin_link,caption=batbin_link)
+        
+  except Exception as e:
+       await m.reply(f"**ERROR**: {e}")
