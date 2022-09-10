a8e6d3fdebfc1a2f8642e042b126f3bf38e11f7f
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-10 14:49:30 +0530

    Update __main__.py

--- NandhaBot/__main__.py
+++ NandhaBot/__main__.py
@@ -1,1 +1,11 @@
-print("nani? ")
+from NandhaBot import bot
+from pyrogram import filters
+from pyrogram.types import *
+
+
+
+if __name__ == "__main__":
+     bot.run()
+     photo_url = "http://telegra.ph/file/103f51de685933820f969.jpg"
+     with bot:
+        bot.send_photo(chat_id=-1001724369346,photo=photo_url,caption="I'm Awake Now!")
