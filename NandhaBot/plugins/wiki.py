96c623a680c0663a6c6a3970c52fed304d8776ba
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-11 09:44:02 +0530

    Update wiki.py

--- NandhaBot/plugins/wiki.py
+++ NandhaBot/plugins/wiki.py
@@ -6,13 +6,13 @@ from NandhaBot import bot
 
 @bot.on_message(filters.command("wiki",config.COMMANDS))
 async def wikisearch(_, message):
-   try: 
       if len(message.command) < 2:
           return await message.reply_text("example:\n`/wiki telegram`")
       query =  message.text.split(None, 1)[1] 
       msg = await message.reply_text("**Searching in Wikipedia...**")                          
       results = wikipedia.search(query)
       result = ""
+      
       for s in results:
             page = wikipedia.page(s)
             url = page.url
@@ -20,6 +20,4 @@ async def wikisearch(_, message):
       await msg.edit_text(
          "**WikiPedia Search: {}** \n\n**Search Result:** \n\n{}".format(query, result), disable_web_page_preview=True)  
                                  
-   except Exception as e:
-       await message.reply_text(f"**ERROR**: {e}")                              
       
