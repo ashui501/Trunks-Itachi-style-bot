522f4cfe09cb65a35f8a71ff79fe01cfcc03f16d
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-11 22:08:47 +0530

    Update help_commands.py

--- NandhaBot/plugins/help_commands.py
+++ NandhaBot/plugins/help_commands.py
@@ -5,9 +5,10 @@ from pyrogram.types import *
 from NandhaBot import bot
 
 HELP_BUTTONS = [[
-  InlineKeyboardButton(text="youtube", callback_data="youtube"),
-  InlineKeyboardButton(text="paste", callback_data="paste"),
-  InlineKeyboardButton(text="telegraph", callback_data="telegraph"),]]
+  InlineKeyboardButton(text="Youtube", callback_data="youtube"),
+  InlineKeyboardButton(text="Paste", callback_data="paste"),
+  InlineKeyboardButton(text="Telegraph", callback_data="telegraph"),],[
+  InlineKeyboardButton(text="Wikipedia ", callback_data="wiki"),]]
 
 BACK_HELP = [[InlineKeyboardButton(text="BACK TO HELP MENU", callback_data="help"),]]
       
@@ -28,4 +29,6 @@ async def helpdata(_, query):
        await query.message.edit_caption(text.PASTE_HELP.format(text.NANDHA),reply_markup=InlineKeyboardMarkup(BACK_HELP))
    elif query.data == "telegraph":
        await query.message.edit_caption(text.TELEGRAPH_HELP.format(text.NANDHA),reply_markup=InlineKeyboardMarkup(BACK_HELP))
+   elif query.data == "wiki":
+       await query.message.edit_caption(text.WIKI_HELP.format(text.NANDHA),reply_markup=InlineKeyboardMarkup(BACK_HELP))
    
