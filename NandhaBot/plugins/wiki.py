2e4e1a1615b9d9720ec8de786adf4d4063a542d3
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-11 21:33:07 +0530

    Update wiki.py

--- NandhaBot/plugins/wiki.py
+++ NandhaBot/plugins/wiki.py
@@ -4,25 +4,26 @@ from pyrogram import filters
 from NandhaBot import bot
 from pyrogram.enums import ParseMode
 
-
 @bot.on_message(filters.command("wiki",config.COMMANDS))
-async def wikisearch(_, message):
-      if len(message.command) < 2:
-          return await message.reply_text("example:\n`/wiki telegram`")
-      query = (
-        message.text.split(None, 1)[1]
-        if len(message.command) < 3
-        else message.text.split(None, 1)[1].replace(" ", "%20")
-    )
-      msg = await message.reply_text("**Searching in Wikipedia...**")                          
-      results = wikipedia.search(query)
-      result = ""
-      
-      for s in results:
+async def wikipediasearch(_, message: Message):
+    reply = message.reply_to_message
+    if len(message.command) < 2:
+          await message.reply_text("example:\n`/wiki telegram`")
+          return 
+    query =  message.text.split(None, 1)[1] 
+    if not query:
+        await message.reply_text("Invalid Syntax see help menu to know how to use this command")
+        return
+    results = wikipedia.search(query)
+    result = ""
+    for s in results:
+        try:
             page = wikipedia.page(s)
             url = page.url
             result += f"> [{s}]({url}) \n"
-      await message.reply_text(
-         "**WikiPedia Search: {}** \n\n**Search Result:** \n\n{}".format(query, result),parse_mode=ParseMode.MARKDOWN ,disable_web_page_preview=True)  
+        except BaseException:
+            pass
+    await message.reply_text(
+        "**WikiPedia Search: {}** \n\n**Result:** \n\n{}".format(query, result), disable_web_page_preview=True)
                                  
       
