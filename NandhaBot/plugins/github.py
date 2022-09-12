1f0c00c2473bdd285309952cf57977902dc40a7e
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-12 06:49:53 +0530

    Update github.py

--- NandhaBot/plugins/github.py
+++ NandhaBot/plugins/github.py
@@ -2,13 +2,16 @@ import os
 import config
 from requests import get
 from pyrogram import filters
+from pyrogram.types import InputMediaPhoto
 from NandhaBot import bot
 
 
 @bot.on_message(filters.command("github",config.COMMANDS))
 async def git(_, message):
     if len(message.command) < 2:
-        return await message.reply_text("gime github username")
+        return await message.reply_text("/github name")
+    photo_url = "https://telegra.ph/file/464250261a5f8a8aaa8b3.jpg"
+    msg = await message.reply_photo(photo_url,"**Result Gathering.....**")
     user = message.text.split(None, 1)[1]
     res = get(f'https://api.github.com/users/{user}').json()
     data = f"""**Name**: {res['name']}
@@ -27,5 +30,5 @@ async def git(_, message):
         kek = get(res['avatar_url']).content
         f.write(kek)
 
-    await message.reply_photo(f"{user}.jpg", caption=data)
+    await msg.edit_media(media=InputMediaPhoto(photo=f"{user}.jpg", caption=data)
     os.remove(f"{user}.jpg")
