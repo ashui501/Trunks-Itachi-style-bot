7756fd1bfbdb7fc1a4696a4b0cb519904025ef32
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-13 07:22:29 +0530

    Create paste.py

---
+++ NandhaBot/helpers/paste.py
@@ -0,0 +1,4 @@
+def spacebin(text):
+    url = "https://spaceb.in/api/v1/documents/"
+    res = post(url, data={"content": text, "extension": "txt"})
+    return f"https://spaceb.in/{res.json()['payload']['id']}"
