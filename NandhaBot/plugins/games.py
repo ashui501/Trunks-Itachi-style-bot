453cb4ca3ef48050348aa7bf347fd8dc0ef9c4b1
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-09-14 07:47:13 +0530

    Update games.py

--- NandhaBot/plugins/games.py
+++ NandhaBot/plugins/games.py
@@ -7,6 +7,7 @@ BUTTON = [[InlineKeyboardButton(text="🔄",callback_data="basketball")]]
 
 @bot.on_message(filters.command("basketball"))
 async def basket(_, message):
+       global id
        id = message.id
        await bot.send_dice(message.chat.id, "🏀",reply_to_message_id=message.id,
                            reply_markup=InlineKeyboardMarkup(BUTTON))
@@ -16,7 +17,7 @@ async def basket(_, message):
 async def games(_, query):                  
     if query.data == "basketball":
         await query.message.delete()
-        await bot.send_dice(message.chat.id, "🏀",reply_to_message_id=id,reply_markup=InlineKeyboardMarkup(BUTTON)
+        await bot.send_dice(message.chat.id, "🏀",reply_to_message_id=id,reply_markup=InlineKeyboardMarkup(BUTTON))
                  
 
 #Truth OR Dare Game
