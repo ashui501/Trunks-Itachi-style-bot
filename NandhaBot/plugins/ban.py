568fbd7dddcec6ff3cb915fa2ab37a750057c5a3
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-01 20:30:48 +0530

    Create ban.py

---
+++ NandhaBot/plugins/ban.py
@@ -0,0 +1,30 @@
+import config
+
+from NandhaBot import bot
+from pyrogram import filters
+from pyrogram.types import *
+
+BANNED_TEXT = "Another one dust Cleared {}!"
+
+@bot.on_message(filters.command("ban",config.COMMANDS))
+def bans(_, message):
+    reply = message.reply_to_message
+    chat_id = message.chat.id
+    if not reply:
+       try:
+         user_id = message.text.replace("/ban", "")
+         bot.ban_chat_member(chat_id, user_id)
+         user_info = bot.get_chat(user_id)
+         name = user_info.first_name
+         message.reply_text(BANNED_TEXT.format(name)
+       except Exception as e:
+            message.reply_text(str(e))
+    elif reply:
+         try:
+           user_id = message.reply_to_message.from_user.id
+           bot.ban_chat_member(chat_id, user_id)
+           user_info = bot.get_chat(user_id)
+           name = user_info.first_name
+           message.reply_text(BANNED_TEXT.format(name)
+         except Exception as e:
+            message.reply_text(str(e))
