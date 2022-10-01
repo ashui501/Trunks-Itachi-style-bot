9b1194bf92e84bca42092095299063a87d53ea97
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-01 19:56:43 +0530

    Update writer.py

--- NandhaBot/plugins/writer.py
+++ NandhaBot/plugins/writer.py
@@ -28,7 +28,18 @@ def write_file(_, query):
         with io.BytesIO(str.encode(write_text)) as file:
              file.name = "writer.txt"
              query.message.reply_document(document=file)
-             query.message.delete()
+             msg.query.message.delete()
     else:
         query.answer("This Message Not For You", show_alert=True)
                  
+@bot.on_callback_query(filters.regex("write_photo"))
+def write_photo(_, query):
+     if query.from_user.id == user_id:
+        msg = query.message.reply_text("Sending File...")
+        write_text = text.replace("/write", "")
+        API = "https://apis.xditya.me/write?text=" + writ_text
+        url = requests.get(API).url
+        query.message.reply_document(document=url)
+        msg.query.message.delete()
+    else:
+        query.answer("This Message Not For You", show_alert=True)
