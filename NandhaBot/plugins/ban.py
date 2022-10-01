035bd7062b422780a4031fbb549dbec9210fbf3f
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-01 20:52:59 +0530

    Update ban.py

--- NandhaBot/plugins/ban.py
+++ NandhaBot/plugins/ban.py
@@ -9,22 +9,24 @@ BANNED_TEXT = "Another one dust Cleared {}!"
 @bot.on_message(filters.command("ban",config.COMMANDS))
 def bans(_, message):
     reply = message.reply_to_message
-    chat_id = message.chat.id
+    chat = message.chat.id
     if not reply:
        try:
           user_id = message.text.replace("/ban", "")
-       except Exception as error:
           user_info = bot.get_chat(user_id)
+       except Exception as error: 
           name = user_info.first_name
-          chat.ban_member(user_id)
+          id_user = user.info.id
+          chat.ban_member(id_user)
           message.reply_text(BANNED_TEXT.format(name))
           message.reply_text(str(error))
     else:
          try:
            user_id = message.reply_to_message.from_user.id
-           chat.ban_member(user_id)
+           user_info = bot.get_chat(user_id)
          except Exception as e:
-            user_info = bot.get_chat(user_id)
             name = user_info.first_name
+            id_user = user_info.id
+            chat.ban_member(id_user)
             message.reply_text(BANNED_TEXT.format(name))
             message.reply_text(str(e))
