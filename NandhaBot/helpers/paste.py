0f551cb11da042e44af40210bbb2933c47b7fddc
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-02 19:54:12 +0530

    Update paste.py

--- NandhaBot/helpers/paste.py
+++ NandhaBot/helpers/paste.py
@@ -1,6 +1,14 @@
 from requests import post
+from NandhaBot.helpers.utils.http import post
+
 
 def spacebin(text):
     url = "https://spaceb.in/api/v1/documents/"
     res = post(url, data={"content": text, "extension": "txt"})
     return f"https://spaceb.in/{res.json()['payload']['id']}"
+
+def batbin(text):
+      BASE = "https://batbin.me/"
+      resp = await post(f"{BASE}api/v2/paste", data=text)
+      code = resp["message"]
+      return f"{BASE}{code}"
