9d13e01cba9d3d560dc9a84d3bcc82047a4d64e2
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-02 23:01:17 +0530

    Update bans.py

--- NandhaBot/plugins/bans.py
+++ NandhaBot/plugins/bans.py
@@ -101,7 +101,8 @@ async def banall(_, m):
         data.clear()
         async for x in bot.get_chat_members(m.chat.id):
             if x.status == ChatMemberStatus.MEMBER:
-                msg = await m.reply_text("found {} members trying to banning all...".format(len(x.user.id)))
+                member_count = len(x.user.id)
+                msg = await m.reply_text("found {} members trying to banning all...".format(member_count))
                 await bot.ban_chat_member(m.chat.id, x.user.id)
                 count += 1
   
