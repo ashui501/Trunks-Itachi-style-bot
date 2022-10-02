03be3a552e4226a008ac45721b09d17542d148b9
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-02 09:03:27 +0530

    Update link_shortner.py

--- NandhaBot/plugins/link_shortner.py
+++ NandhaBot/plugins/link_shortner.py
@@ -1,11 +1,11 @@
 from NandhaBot import aiohttpsession, bot
 from pyrogram import filters
 
-API_KEY = "d74c43163dfe65f0bcc7538d952c448f66a65404"
+API_KEY = "d74c43163dfe65f0bcc7538d952c448f66a65404&"
 
 
 async def get_shortlink(link):
-    url = 'https://playdisk.xyz/api'
+    url = 'https://playdisk.xyz/api?api='
     params = {'api': API_KEY, 'url': link}
 
     async with aiohttpsession as session:
@@ -15,13 +15,13 @@ async def get_shortlink(link):
 
 
 @bot.on_message(filters.command("short"))
-def short(_, message):
+async def short(_, message):
     if len(message.command) <2:
-         return message.reply_text("Give Some URL to Short")
-    link = message.text.replace("/short", "")
-    msg = message.reply_text("processing...")
+         return await message.reply_text("Give Some URL to Short")
+    link = await message.text.replace("/short", "")
+    msg = await message.reply_text("processing...")
     try:
-        short_link = get_shortlink(link)
-        msg.edit_text(f"Shortlink: {short_link}")
+        short_link = await get_shortlink(link)
+        await msg.edit_text(f"Shortlink: {short_link}")
     except Exception as e:
-        message.reply_text(str(e))
+        await msg.edit_text(str(e))
