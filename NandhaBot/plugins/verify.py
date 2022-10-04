634c8d1e8ed9741c27be039846c08966fa4be4dd
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-04 10:54:17 +0530

    Update verify.py

--- NandhaBot/plugins/verify.py
+++ NandhaBot/plugins/verify.py
@@ -17,13 +17,13 @@ async def res(client, message):
              key = InlineKeyboardMarkup([[InlineKeyboardButton("BAN", callback_data=f"botban:{member.id}"),
                     InlineKeyboardMarkup([[InlineKeyboardButton("UNMUTE", callback_data=f"botunm:{member.id}"),]])
              await message.reply_text("BOT ARRIVED ON CHAT",reply_markup=key)
-         if is_bot == "False":
+         else:
               try:
                  await bot.restrict_chat_member(message.chat.id, member.id, ChatPermissions(can_send_messages=False))
                  key = InlineKeyboardMarkup([[InlineKeyboardButton("I'm a human", callback_data=f"unres:{member.id}")]])
                  await message.reply(f"Hello ( {member.mention} ) You are restricted to make sure you are not a robot", reply_markup=key)
               except Exception as e:
-                await message.reply_text(str(e))
+                 await message.reply_text(str(e))
 
 
 @bot.on_callback_query(filters.regex("batban"))
