0326a621e7c9cca47d269a501d8106d116cf136b
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-04 21:32:45 +0530

    Update pin_messages.py

--- NandhaBot/plugins/pin_messages.py
+++ NandhaBot/plugins/pin_messages.py
@@ -20,7 +20,7 @@ def pin(_, message):
       user_stats = bot.get_chat_member(chat_id, user_id)
       if message.chat.type == enums.ChatType.PRIVATE:
             return message.reply_text("work only on groups!")
-      elif user_stats.privileges.can_pin_messages not message.reply_to_message:
+      elif user_stats.privileges.can_pin_messages and not message.reply_to_message:
          
           try:
             message_id = message.text.split(None,1)[1]
