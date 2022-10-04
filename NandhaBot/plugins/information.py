94d006d1f461b21e43b6bbd6ee52d569e700e9cc
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-04 23:10:26 +0530

    Update information.py

--- NandhaBot/plugins/information.py
+++ NandhaBot/plugins/information.py
@@ -24,7 +24,7 @@ def userinfo(_, message):
      chat_id = message.chat.id
      user_id = message.from_user.id
      if not message.reply_to_message:
-         user_id = int(message.text.split(None, 1)[1])
+         user_id = int(message.text.split(None, 1)[1]) or user_id
          try:
             user_info = bot.get_chat(user_id)
             user = bot.get_users(user_id)
@@ -38,19 +38,4 @@ def userinfo(_, message):
 id,name, username, mention, dc_id, bio))
          except Exception as e:
               message.reply_text(str(e))
-     else:
-         try:
-            user_info = bot.get_chat(user_id)
-            user = bot.get_users(user_id)
-            id = user_info.id
-            dc_id = user.dc_id
-            name = user_info.first_name
-            username = user_info.username
-            mention = user.mention
-            bio = user_info.bio
-            message.reply_text(INFO_TEXT.format(
-id,name, username, mention, dc_id, bio))
-         except Exception as e:
-              message.reply_text(str(e))
-         
-          
+    
