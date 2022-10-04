709fcaa204ecdffbd687f28dbdc133c978453a2b
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-04 13:31:35 +0530

    Create admins.py

---
+++ NandhaBot/plugins/admins.py
@@ -0,0 +1,16 @@
+import config
+
+from pyrogram.enums import ChatType
+from pyrogram import filters
+from NandhaBot import bot
+
+
+@bot.on_message(filters.command("admins",config.COMMANDS))
+async def admins(_, message):
+      if message.chat.type == ChatType.PRIVATE:
+           await message.reply_text("This command work on group only!")
+      else:
+        admin_list = []
+        async for admin in bot.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+             admin_list.append(admin.first_name)
+        await message.reply_text(str(admin_list)
