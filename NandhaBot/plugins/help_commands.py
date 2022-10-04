286cf2f30483925de017bbc053c9efbe216086ce
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-04 17:36:04 +0530

    Update help_commands.py

--- NandhaBot/plugins/help_commands.py
+++ NandhaBot/plugins/help_commands.py
@@ -5,17 +5,38 @@ from pyrogram import filters
 from pyrogram.types import *
 from NandhaBot import bot
 
-HELP_BUTTONS = [[
+
+#buttons
+
+HELP_BACK_BUTTONS = [[
   InlineKeyboardButton(text="Misc help", callback_data="misc_help")]]
 
 BACK_HELP = InlineKeyboardMarkup([[InlineKeyboardButton(text="BACK TO HELP MENU", callback_data="help")]])
       
    
+help_button = InlineKeyboardMarkup([[
+InlineKeyboardButton("OPEN IN DM",url="https://t.me/trunksRobot?start"),
+InlineKeyboardButton("OPEN IN GROUP",callback_data="help_back")]])
+
+
+#commads
 
 @bot.on_message(filters.command("help",config.COMMANDS))
 def help(_, message):
-     message.reply_text(strings.HELP_TEXT,reply_markup=InlineKeyboardMarkup(HELP_BUTTONS))
+    if message.chat.type == enums.ChatType.PRIVATE:
+        message.reply_text(strings.HELP_TEXT,reply_markup=HELP_BACK_BUTTONS)
+    else:
+        message.reply_text(strings.HELP_GROUP_TEXT,reply_markup=help_button)
+
+
       
-@bot.on_callback_query(filters.regex("misc_help"))
+#callbackdatas
+
+
+
+@bot.on_callback_query(filters.regex("help_back"))
 def helpdatas(_, query):
-        query.message.edit(strings.MISC_HELP.format(strings.NANDHA),reply_markup=BACK_HELP)
+        def help(_, message):
+     message.reply_text(strings.HELP_TEXT,reply_markup=help_button)
+
+
