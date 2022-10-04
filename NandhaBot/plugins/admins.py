63be87feac8084f250ca2bc2344db01a8a05b1ee
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-04 14:30:28 +0530

    Update admins.py

--- NandhaBot/plugins/admins.py
+++ NandhaBot/plugins/admins.py
@@ -12,7 +12,7 @@ async def admins(_, message):
       if message.chat.type == ChatType.PRIVATE:
            await message.reply_text("This command work on group only!")
       else:
-        admin_list = "**total admins in {message.chat.title}**:\n"
+        admin_list = f"**total admins in {message.chat.title}**:\n"
         async for admin in bot.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
                    admin_list += f"✮ {admin.user.mention}\n"
         await message.reply_text(admin_list)
