8e109d979fd2e5eeb623e91c4ead77c62cc56ed6
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-04 17:17:07 +0530

    Update help_commands.py

--- NandhaBot/plugins/help_commands.py
+++ NandhaBot/plugins/help_commands.py
@@ -6,13 +6,7 @@ from pyrogram.types import *
 from NandhaBot import bot
 
 HELP_BUTTONS = [[
-  InlineKeyboardButton(text="Youtube", callback_data="youtube"),
-  InlineKeyboardButton(text="Paste", callback_data="paste"),
-  InlineKeyboardButton(text="Telegraph", callback_data="telegraph"),],[
-  InlineKeyboardButton(text="Wiki", callback_data="wiki"),
-  InlineKeyboardButton(text="Github", callback_data="github"),
-  InlineKeyboardButton(text="Game", callback_data="game"),
-  InlineKeyboardButton(text="Nekos", callback_data="nekos")]]
+  InlineKeyboardButton(text="Misc help", callback_data="misc_help")]]
 
 BACK_HELP = [[InlineKeyboardButton(text="BACK TO HELP MENU", callback_data="help"),]]
       
@@ -22,7 +16,6 @@ BACK_HELP = [[InlineKeyboardButton(text="BACK TO HELP MENU", callback_data="help
 def help(_, message):
      message.reply_text(strings.HELP_TEXT,reply_markup=InlineKeyboardMarkup(HELP_BUTTONS))
       
-@bot.on_callback_query(filters.regex("misc"))
+@bot.on_callback_query(filters.regex("misc_help"))
 def helpdatas(_, query):
-     if query.data == "paste":
-        query.message.edit(strings.PASTE_HELP)
+        query.message.edit(strings.MISC_HELP.format(strings.NANDHA))
