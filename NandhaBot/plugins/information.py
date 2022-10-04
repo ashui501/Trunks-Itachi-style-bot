ee179249ee0ab6856ccf0a9f68febd861bc3f904
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-04 23:06:07 +0530

    Update information.py

--- NandhaBot/plugins/information.py
+++ NandhaBot/plugins/information.py
@@ -16,7 +16,7 @@ INFO_TEXT = """
 **DC ID:** {}
 **Bio:** {}
 
-`note were sends you necessary information about you not at all`!
+`note were sends you necessary information about user not at all`!
 """
 @bot.on_message(filters.command(["info","userinfo"],config.COMMANDS))
 def userinfo(_, message):
@@ -38,4 +38,19 @@ def userinfo(_, message):
 id,name, username, mention, dc_id, bio))
          except Exception as e:
               message.reply_text(str(e))
+     else:
+         try:
+            user_info = bot.get_chat(user_id)
+            user = bot.get_users(user_id)
+            id = user_info.id
+            dc_id = user.dc_id
+            name = user_info.first_name
+            username = user_info.username
+            mention = user.mention
+            bio = user_info.bio
+            message.reply_text(INFO_TEXT.format(
+id,name, username, mention, dc_id, bio))
+         except Exception as e:
+              message.reply_text(str(e))
+         
           
