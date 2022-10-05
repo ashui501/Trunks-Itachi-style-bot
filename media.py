02d2760e478254d37716fdb5129f78c59151ce64
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-06 01:13:36 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,3 +1,10 @@
+TRUNKS = [
+"https://telegra.ph/file/b2bbd3104c482488ce3ba.jpg",
+"https://telegra.ph/file/033d1e65cdb5e52822b42.jpg",
+"https://telegra.ph/file/bece8141bd626a7ab1478.jpg",
+"https://telegra.ph/file/adb286419450bbfb06851.jpg"
+]
+
 BAN_IMG = ""
 START_IMG = ""
 GROUP_START_IMG = ""
