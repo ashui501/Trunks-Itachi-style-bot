22d2b3cad19e5eadd1a4b13b1e74daffe953fad3
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-05 09:08:59 +0530

    Update bans.py

--- NandhaBot/plugins/bans.py
+++ NandhaBot/plugins/bans.py
@@ -24,7 +24,7 @@ def bans(_, message):
     admin = message.from_user.id
     if message.chat.type == ChatType.PRIVATE:
           return message.reply_text("this command work only on groups")
-    elif not reply and len(message.command) != 1:
+    elif not reply and len(message.command) == 1:
     
        try:
           user_id = str(message.text.split(None, 1)[1])
