aff28175c9fbb9983e41440b71828cff135f027e
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-05 22:47:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ USERNAME = "TrunksRobot"
 ID = 5515671520
 COMMANDS = [".","?","/","!","$","#","~",]
 GROUP_ID = -1001717881477
+LOG_CHANNEL_ID = -1001622628177
 GROUP_URL = "t.me/nandhasupport"
 CHANNEL_ID = -1001566403901
 CHANNEL_URL = "t.me/nandhabots"
