214a5219284d5406facf8ce0a1ae59951be8e1dc
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-05 22:13:38 +0530

    Update information.py

--- NandhaBot/plugins/information.py
+++ NandhaBot/plugins/information.py
@@ -3,7 +3,7 @@ import config
 from pyrogram import filters
 from pyrogram import enums
 from NandhaBot import bot
-
+from NandhaBot.rank import RANK_USERS as RANK
 
 INFO_TEXT = """
 𝗨𝗦𝗘𝗥𝗦𝗧𝗔𝗧𝗨𝗦:
@@ -14,13 +14,14 @@ INFO_TEXT = """
 **Mention:** {}
 
 **UserStatus:**\n`{}`
-\n**Dc:** {}
+**RankUser**: {}
+**Dc:** {}
 **Bio:** {}
 
 `note were sends you necessary information about user not at all`!
 """
 
-def userstatus(user_id):
+async def userstatus(user_id):
    try:
       user = bot.get_users(user_id)
       x = user.status
@@ -41,7 +42,7 @@ def userstatus(user_id):
 
 
 @bot.on_message(filters.command(["info","userinfo"],config.COMMANDS))
-def userinfo(_, message):
+async def userinfo(_, message):
     
      chat_id = message.chat.id
      user_id = message.from_user.id
@@ -49,9 +50,10 @@ def userinfo(_, message):
          
          try:
             user_id = str(message.text.split(None, 1)[1])
-            user_info = bot.get_chat(user_id)
-            user = bot.get_users(user_id)
-            status = userstatus(user.id)
+            user_info = await bot.get_chat(user_id)
+            user = await bot.get_users(user_id)
+            rank = user.id in (await RANK())
+            status = await userstatus(user.id)
             id = user_info.id
             dc_id = user.dc_id
             name = user_info.first_name
@@ -59,41 +61,43 @@ def userinfo(_, message):
             mention = user.mention
             bio = user_info.bio
             photo = bot.download_media(user_info.photo.big_file_id)
-            bot.send_photo(chat_id,photo=photo, caption=INFO_TEXT.format(
-id,name, username, mention, status,dc_id, bio),reply_to_message_id=message.id)
+            await bot.send_photo(chat_id,photo=photo, caption=INFO_TEXT.format(
+id,name, username, mention, status, rank,dc_id, bio),reply_to_message_id=message.id)
          except Exception as e:
-              message.reply_text(str(e))
+              await message.reply_text(str(e))
     
      elif not message.reply_to_message and len(message.command) == 1:
          try:
-            user_info = bot.get_chat(user_id)
-            user = bot.get_users(user_id)
-            status = userstatus(user.id)
+            user_info = await bot.get_chat(user_id)
+            user = await bot.get_users(user_id)
+            status = await userstatus(user.id)
             id = user_info.id
             dc_id = user.dc_id
+            rank = user.id in (await RANK())
             name = user_info.first_name
             username = user_info.username
             mention = user.mention
             bio = user_info.bio
-            photo = bot.download_media(user_info.photo.big_file_id)
-            bot.send_photo(chat_id,photo=photo, caption=INFO_TEXT.format(
-id,name, username, mention,status, dc_id, bio),reply_to_message_id=message.id)
+            photo = await bot.download_media(user_info.photo.big_file_id)
+            await bot.send_photo(chat_id,photo=photo, caption=INFO_TEXT.format(
+id,name, username, mention,status,rank, dc_id, bio),reply_to_message_id=message.id)
          except Exception as e:
-              message.reply_text(str(e))
+              await message.reply_text(str(e))
      elif message.reply_to_message:
           user_id = message.reply_to_message.from_user.id
           try:
-            user_info = bot.get_chat(user_id)
-            user = bot.get_users(user_id)
-            status = userstatus(user.id)
+            user_info = await bot.get_chat(user_id)
+            user = await bot.get_users(user_id)
+            status = await userstatus(user.id)
             id = user_info.id
             dc_id = user.dc_id
+            rank = user.id in (await RANK())
             name = user_info.first_name
             username = user_info.username
             mention = user.mention
             bio = user_info.bio
-            photo = bot.download_media(message.reply_to_message.from_user.photo.big_file_id)
-            bot.send_photo(chat_id,photo=photo,caption=INFO_TEXT.format(
-id,name, username, mention,status, dc_id, bio),reply_to_message_id=message.id)
+            photo = await bot.download_media(message.reply_to_message.from_user.photo.big_file_id)
+            await bot.send_photo(chat_id,photo=photo,caption=INFO_TEXT.format(
+id,name, username, mention,status,rank, dc_id, bio),reply_to_message_id=message.id)
           except Exception as e:
-              message.reply_text(str(e))
+              await message.reply_text(str(e))
