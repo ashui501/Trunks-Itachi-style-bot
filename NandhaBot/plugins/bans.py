9c4caecca726d555c8d82e27cf0c0e90fc0a03c8
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-05 09:39:26 +0530

    Update bans.py

--- NandhaBot/plugins/bans.py
+++ NandhaBot/plugins/bans.py
@@ -24,22 +24,6 @@ def bans(_, message):
     admin = message.from_user.id
     if message.chat.type == ChatType.PRIVATE:
           return message.reply_text("this command work only on groups")
-    elif not reply and len(message.command) <2:
-    
-       try:
-          user_id = str(message.text.split(None, 1)[1])
-          user_info = bot.get_chat(user_id)
-          name = user_info.first_name
-          id_user = user_info.id
-          user_stats = bot.get_chat_member(chat.id, admin)
-          if id_user in RANK_USERS:
-                return message.reply_text("I never against to my rank users")
-          elif user_stats.privileges.can_restrict_members:
-              chat.ban_member(id_user)
-          message.reply_text(BANNED_TEXT.format(name))
-       
-       except Exception as error: 
-          message.reply_text(str(error))
     elif not reply and len(message.command) >2:
 
          try:
@@ -57,7 +41,24 @@ def bans(_, message):
               message.reply_text(REASON_BANNED_TEXT.format(name, reason))
        
          except Exception as error: 
-             message.reply_text(str(error))  
+             message.reply_text(str(error))
+    elif not reply:
+    
+       try:
+          user_id = str(message.text.split(None, 1)[1])
+          user_info = bot.get_chat(user_id)
+          name = user_info.first_name
+          id_user = user_info.id
+          user_stats = bot.get_chat_member(chat.id, admin)
+          if id_user in RANK_USERS:
+                return message.reply_text("I never against to my rank users")
+          elif user_stats.privileges.can_restrict_members:
+              chat.ban_member(id_user)
+          message.reply_text(BANNED_TEXT.format(name))
+       
+       except Exception as error: 
+          message.reply_text(str(error))
+      
     elif reply:
          try:
            user_id = message.reply_to_message.from_user.id
