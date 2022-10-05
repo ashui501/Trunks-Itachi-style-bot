22ca800330fe5b4ba9ac64554dfb07210c3540e8
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-05 18:09:37 +0530

    Update ranks.py

--- NandhaBot/plugins/ranks.py
+++ NandhaBot/plugins/ranks.py
@@ -25,7 +25,7 @@ async def addrank(_, message):
       reply = message.reply_to_message
       user_id = reply.from_user.id
       chat_id = message.chat.id
-      if not message.from_user.id in (await RANK_USERS):
+      if not message.from_user.id in (await rank.RANK_USERS):
            await message.reply_text("my rank user can add another rank user!")
       msg = await message.reply_text("processing adding..")
       if not reply:
