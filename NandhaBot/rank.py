09b5e53c1facdc55ffab3a3c5d0e9f454b183b2b
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-05 16:38:32 +0530

    Update rank.py

--- NandhaBot/rank.py
+++ NandhaBot/rank.py
@@ -2,11 +2,15 @@
 
 from NandhaBot.helpers.ranksdb import get_rankusers
 
-RANK_USERS = str(get_rankusers())
+
+
+
+async def RANK_USERS() ->list:
+     return str(await get_rankusers())
 
 RANK_A_USER = [1491497760,5696053228]
 RANK_B_USER = []
 RANK_C_USER = []
 
-RANK_USERS = (RANK_A_USER+RANK_B_USER+RANK_C_USER)
+
 RANK_AB_USER = (RANK_A_USER+RANK_B_USER)
