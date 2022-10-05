3a61fbcda568e8219f4f495b1ba6f250d7dde944
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-05 20:05:49 +0530

    Create gbandb.py

---
+++ NandhaBot/helpers/gbandb.py
@@ -0,0 +1,22 @@
+from NandhaBot import mongodb
+
+collection = mongodb["GBAN"]
+
+
+async def gban_user(chat):
+    doc = {"_id": "Gban", "users": [chat]}
+    r = await collection.find_one({"_id": "Gban"})
+    if r:
+        await collection.update_one({"_id": "Gban"}, {"$push": {"users": chat}})
+    else:
+        await collection.insert_one(doc)
+
+async def get_gbaned_users():
+    results = await collection.find_one({"_id": "Gban"})
+    if results:
+        return results["users"]
+    else:
+        return []
+
+async def ungban_user(chat):
+    await collection.update_one({"_id": "Gban"}, {"$pull": {"users": chat}})
