83c0a63c13d09a627f8a215582070e73b63724f0
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-06 15:30:31 +0530

    Update __init__.py

--- NandhaBot/__init__.py
+++ NandhaBot/__init__.py
@@ -21,13 +21,20 @@ LOGGER = logging.getLogger(__name__)
 API_ID = os.environ.get("API_ID", 7126006)
 API_HASH = os.environ.get("API_HASH", "f92b05be529835381859ead64a195fa2")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5515671520:AAFcuSq058VM8QmShpweRs1p0u0U5pWHIH8")
+SESSION = os.environ.get("SESSION", "BQBSaiIAo99yvNLnCudpQWAa8L2ivVFdbWzJt27Khgr-sMpVHSRcdjNfYD8J8zBh5jChyc55xZSaNjEMhtwMOGYzqs6e0CC8Bgrghuu5hH2HCre-AAyXHs2EglEsrnrXWsrkrFgLJ2tDgK2IEG4QPvRxlbaDrvCZ-40V_krnLIVaxcMj5w5wiSrUMWHuUCYnNtITPUTx8ap8B5FHcjuyxXMNVnvr1tSDcLWztn-KbhexULPUIZWdvpVDEBR-ONGH95VdSI12HiC8L03BeBowJd4sbW75NO7rfgjpG0ouSxdl31wB5MW8cvaBzY6YAHSJwmc3BgytxcnFUktw2HdxLvn9g4SX4gAAAAFIwnfgAQ")
 
+### pyrogram Client Run Codes ###
 
-
-### pyrogram Client Run Code ###
 plugins = dict(root="NandhaBot")
 
-bot = Client(name="nandhabot", 
+user = Client(name="NandhaxD",
+              api_id=int(API_ID),
+              api_hash=str(API_HASH),
+              bot_token=str(BOT_TOEKN),
+              session_string=str(SESSION),
+              plugins=plugins)
+
+bot = Client(name=str(config.USERNAME), 
              api_id=API_ID, 
              api_hash=API_HASH,
              bot_token=BOT_TOKEN,
