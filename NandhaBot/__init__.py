8901cf306f2eca1caba888a40902ac46528afbbc
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-07 12:28:34 +0530

    Update __init__.py

--- NandhaBot/__init__.py
+++ NandhaBot/__init__.py
@@ -1,7 +1,7 @@
 import config
 
 from pyrogram import filters , Client
-from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+from pymongo MongoClient
 from aiohttp import ClientSession
 import logging
 import time
