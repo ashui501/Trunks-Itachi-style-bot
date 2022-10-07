a34d8b2fbd64317d06386ea71079a58506def3a0
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-07 14:13:20 +0530

    Update groupsdb.py

--- NandhaBot/helpers/groupsdb.py
+++ NandhaBot/helpers/groupsdb.py
@@ -2,20 +2,17 @@ from NandhaBot import pymongodb
 
 chatsdb = pymongodb.GROUPSDB
 
-async def is_chats(chat_id: int) -> bool:
-    chat = chatsdb.find_one({"chat_id": chat_id})
-    if not chat:
-        return False
-    return True
 
-async def get_chats() -> list:
+
+
+async def get_chats():
     chats_list = []
-    for chat in chatsdb.find({"chat_id": {"$gt": 0}}):
-        chats_list.append(chat)
+    for chats in groupsdb.find():
+        chats_list.append(chats["chat_id"])
     return chats_list
 
 async def add_chats(chat_id: int):
-    is_already_in_chatdb = await is_chats(chat_id)
-    if is_already_in_chatdb:
-        return
-    return chatsdb.insert_one({"chat_id": chat_id})
+     if not chat_id in (await get_chats()):
+           return chatsdb.insert_one({"chat_id": chat_id})
+
+
