9ec8a8bc1f626edd95ff8351c7dfa35626f29724
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-07 20:43:40 +0530

    Update scandb.py

--- NandhaBot/helpers/scandb.py
+++ NandhaBot/helpers/scandb.py
@@ -11,11 +11,7 @@ async def add_scan_user_details(user_id: str, reason, proof):
           return scandb.insert_one({user_id: details})
 
 
-async def get_scan_user_details(user_id: str):
-    list = []
-    user = scandb.find_one({"user_id":  user_id})
+get_scan_user_details(user_id: str):
+    user = await scandb.find_one({"user_id":  user_id})
     if user:
-        list.append(user)
-    return list
-
-
+        return user[user_id]
