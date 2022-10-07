4da8bb044f38d91d36373b2459730bce30329ef0
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-07 23:11:30 +0530

    Update and rename scandb.py to scansdb.py

---
+++ NandhaBot/helpers/scansdb.py
@@ -0,0 +1,12 @@
+from NandhaBot import pymongodb
+
+scandb = pymongodb.SCANSDBS
+
+
+
+async def add_scan_user_details(user_id: str, reason):
+          scan_reason_list = { "user_id": user_id, "reason": reason}
+          x = scan.insert_one(scan_reason_list)
+          id = x.inserted_id
+          return id
+        
