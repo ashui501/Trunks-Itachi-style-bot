81cb699c832a10a0d99d5990e5238d61bb65c29d
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-07 18:06:06 +0530

    Update scandb.py

--- NandhaBot/helpers/scandb.py
+++ NandhaBot/helpers/scandb.py
@@ -8,5 +8,5 @@ async def add_scan_user_details(user_id, reason, proof):
           details = {"user_id": user_id,
                      "reason": reason,
                      "proof": proof}
-          return details.insert_one({"{user_id}": details})
+          return details.insert_one({user_id: details})
 
