5e4468898e7f8bcc5dfda4115d892506ac6a8c18
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-08 13:04:13 +0530

    Update scansdb.py

--- NandhaBot/helpers/scansdb.py
+++ NandhaBot/helpers/scansdb.py
@@ -15,3 +15,9 @@ async def get_details(user_id: int):
 async def get_scan_users():
       for user_ids in scansdb.find():
           return [user_ids["_id"]]
+
+async def is_scanned(user_id: int)
+      scan_user_list = await get_scan_users()
+      if not user_id in scan_user_list:
+           return False
+      return True
