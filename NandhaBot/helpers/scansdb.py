a18081927332e7014efca2e5dc3d7192e6bd6edc
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-08 12:04:07 +0530

    Update scansdb.py

--- NandhaBot/helpers/scansdb.py
+++ NandhaBot/helpers/scansdb.py
@@ -11,5 +11,10 @@ async def add_details(user_id: int, reason: str, proof: str):
 async def get_details(user_id: int):
          details = scansdb.find_one({"_id": user_id})
          return details
-          
-        
+         
+
+async def update_details(user_id: int, reason: str, proof: str):
+        details = await get_details(user_id)
+        scan_reason_list = {"_id": user_id, "user_id": user_id, "reason": reason, "proof": proof}
+        details = scansdb.update_one(details, scan_reason_list)
+        return "`Successfully Updated Reasons.`"
