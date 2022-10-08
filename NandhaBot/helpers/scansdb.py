b0371c5dd5cc6b780eeed76af04176a5bd71e689
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-08 09:58:23 +0530

    Update scansdb.py

--- NandhaBot/helpers/scansdb.py
+++ NandhaBot/helpers/scansdb.py
@@ -4,13 +4,12 @@ scansdb = pymongodb.SCANSDBS
 
 
 
-
-async def add_details(user_id, reason: str):
-          scan_reason_list = {"user_id": user_id, "reason": reason}
-          scansdb.insert_one(scan_reason_list)
+aync def add_details(user_id: int, reason: str):
+                 scan_reason_list = {"_id": user_id, "user_id": user_id, "reason": reason}
+                 scansdb.insert_one(scan_reason_list)
 
 async def get_details(user_id):
-         details = scansdb.find_one({"user_id": user_id})
+         details = scansdb.find_one({"_id": user_id})
          return details 
 
           
