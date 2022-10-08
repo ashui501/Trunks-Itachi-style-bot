d1ab8c68bfb4e483ac5919614d4580e02687276d
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-08 16:53:42 +0530

    Update scan.py

--- NandhaBot/scanner/scan.py
+++ NandhaBot/scanner/scan.py
@@ -29,14 +29,14 @@ async def scan(_, message):
       if not message.from_user.id in (await RANK_USERS()):
           return await msg.edit("`you don't enough rights to use me.`")
       elif reply or not reply and len(message.text) <2:
-          return msg.edit("`you need to use correct /formatting scanning someone else.`")
+          return await msg.edit("`you need to use correct /formatting scanning someone else.`")
       elif reply:
          try:
             user_id = reply.from_user.id
             reason = message.text.split("-r")[1].split("-p")[0]
             proof = message.text.split("-p")[1]
             if user_id in (await is_scan_user(user_id)) == True:
-               await msg.edit("`your trying to scan someone but that user already scanned.`")
+                 return await msg.edit("`your trying to scan someone but that user already scanned.`")
             else:
                 await add_scan_user(user_id,reason,proof)
                 await msg.edit("the user successfully 𝗦𝗖𝗔𝗡𝗡𝗘𝗗!")
