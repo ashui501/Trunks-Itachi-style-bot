770b407993d8cbd7ab494ebf096353d08a3c61ff
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-08 10:09:41 +0530

    Update scansdb.py

--- NandhaBot/helpers/scansdb.py
+++ NandhaBot/helpers/scansdb.py
@@ -8,7 +8,7 @@ async def add_details(user_id: int, reason: str):
                  scan_reason_list = {"_id": user_id, "user_id": user_id, "reason": reason}
                  scansdb.insert_one(scan_reason_list)
 
-async def get_details(user_id):
+async def get_details(user_id: int):
          details = scansdb.find_one({"_id": user_id})
          return details 
 
