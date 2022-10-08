0c30d43f004e5a73771b0681abf968b548bceb09
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 00:14:56 +0530

    Update scan.py

--- NandhaBot/scanner/scan.py
+++ NandhaBot/scanner/scan.py
@@ -19,6 +19,15 @@ scanned user: {}
 reason: {}
 """
 
+CHECK_TEXT = """
+SCANNED USER:
+
+scanned id: {}
+the reason for scan: 
+{}
+
+scanned date: {}
+"""
 @bot.on_message(filters.command("scan",config.COMMANDS))
 async def scan(_, message):
       reply = message.reply_to_message
@@ -60,23 +69,45 @@ async def scan(_, message):
                await msg.edit(str(e))
       
       
-@bot.on_message(filters.command("addproof"))
+@bot.on_message(filters.command("addproof",config.COMMANDS))
 async def addproof(_, message):
       reply = message.reply_to_message
       date = message.date
       msg = await message.reply_text("`adding proof...`")
       if not message.from_user.id in (await RANK_USERS()):
-           await msg.edit("`you don't have enough rights to use me.`")
-      elif not reply:
+           return await msg.edit("`you don't have enough rights to use me.`")
+      elif len(message.command) <2:
+           return await msg.edit("`use a correct format for add proof.`")
+      else:  
           try:           
              user_id = int(message.text.split("-u")[1].split("-p")[0])
              proof = message.text.split("-p")[1]
              if not user_id in (await get_scan_users()):
                  return await msg.edit("`this user not a scanned user to add proof.`")
-             elif not "http" == proof:
-                  return await msg.edit("`make you sure proof as a telegraph.`")
+
              await update_scan_proof(user_id,proof,date)
              await msg.edit("`Successfully proof added!`")   
           except Exception as e:
               await msg.edit(str(e))
+
+@bot.on_message(filters.commamd("check",config.COMMANDS))
+async def check(_, message):
+       reply = message.reply_to_message
+       if not message.from_user.id in (await RANK_USERS()):
+            return await message.reply_text("`your don't have enough rights to use me.`")
+       elif len(message.command) <2:
+            return await message.reply_text("`use a correct format for check user.`")
+       elif reply:
+            user_id = int(reply.from_user.id)
+            if (await is_scan_user(user_id)) == False:
+                  return await message.reply_text("`This user not scanned.`")
+            else:
+                details = get_scan_user(user_id)
+                user_id = details["user_id"]
+                reason = details["reason"]
+                date = details["date"]
+                proof = details["proof"]
+                await bot.send_document(message.chat.id, 
+                      document=proof,
+                      caption=CHECK_TEXT.format(user_id,reason,date))
               
