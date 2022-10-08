1f784391ab26311a631a9d18529872ba9a0ec1cb
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-08 16:59:22 +0530

    Update scan.py

--- NandhaBot/scanner/scan.py
+++ NandhaBot/scanner/scan.py
@@ -30,7 +30,7 @@ async def scan(_, message):
           return await msg.edit("`you don't enough rights to use me.`")
       elif reply or not reply and len(message.command) <2:
           return await msg.edit("`you need to use correct /formatting scanning someone else.`")
-      elif reply:
+      elif reply and len(message.command) >2:
          try:
             user_id = reply.from_user.id
             reason = message.text.split("-r")[1].split("-p")[0]
