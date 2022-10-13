f22c28c7fc163654afaf9c10396ac607ca5868eb
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-13 23:07:24 +0530

    Update link_shortner.py

--- NandhaBot/plugins/link_shortner.py
+++ NandhaBot/plugins/link_shortner.py
@@ -5,15 +5,16 @@ from NandhaBot import bot
 from pyrogram import filters
 
 
-@bot.on_message(filters.command("short",config.COMMANDS))
+@bot.on_message(filters.command("getlink",config.COMMANDS))
 async def short(_, message):
-    if len(message.command) <2:
-         return await message.reply_text("Give Some URL to Short")
-    link = message.text.split(None, 1)[1]
+    if message.reply_to_message:
+         link = message.reply_to_message.text
+    elif not message.reply_to_message:
+         link = message.text.split(None, 1)[1]
     msg = await message.reply_text("processing...")
     try:
         search = pyshorteners.Shortener()
         short_url = search.tinyurl.short(link)
-        await msg.edit_text(f"Shortlink: {short_url}")
+        await msg.edit_text(f"**shorteners link**:\n`{short_url}`")
     except Exception as e:
         await msg.edit_text(str(e))
