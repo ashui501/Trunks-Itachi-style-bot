aa17177b0e8cc4378c48bf40e01a9e6de1c04555
Ashui <113001130+ashui501@users.noreply.github.com>
2023-10-02 21:01:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-USERNAME = "TrunksRobot"
+USERNAME = "Trunksprobot"
 BOT_ID = 5515671520
 COMMANDS = [".","?","/","!","$","#","~",]
 GROUP_ID = -1001724369346
 LOG_CHANNEL_ID = -1001622628177
-GROUP_URL = "t.me/TrunksSupport"
+GROUP_URL = "t.me/TrunksbotSupport"
 CHANNEL_ID = -1001566403901
-CHANNEL_URL = "t.me/TrunksUpdates"
+CHANNEL_URL = "t.me/TrunksbotUpdates"
 
